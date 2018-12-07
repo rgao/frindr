04815018ac1fd208e3d1a5b29840124ce1330ff7
rgao <xinghoshi@gmail.com>
2018-12-06 22:57:53 -0800

    changed default page to try to fix deployment

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,7 +10,7 @@ function domain(app) {
         response.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
-    app.get("/", function(request, response) {
+    app.get("*", function(request, response) {
         response.sendFile(path.join(__dirname, "../public/home.html"));
     });
 }
