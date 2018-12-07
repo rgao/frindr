325d510c03cde365b9c28996866f6ff8eadffcf8
rgao <xinghoshi@gmail.com>
2018-12-06 23:12:13 -0800

    attempt to fix css deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 var app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(path.join(__dirname + "/app")));
+app.use(express.static(path.join(__dirname + "/app/public/")));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
